02c9bbfe838b67ae0e0c930e6c984f71c96d1498
Sk-Kalesha <kaleemuddeen.shaik@gmail.com>
2022-11-16 12:52:02 +0000

    React-Assignment-1-Browser

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import WebHistorySearch from './components/WebHistorySearch'
+
+import './App.css'
+
+// These are the list used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <WebHistorySearch />
+
+export default App
